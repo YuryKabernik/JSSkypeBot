f46132bb3c1c6d328f333ba2939cfa578e68488e
KobernykYury <yury.kabernik.berazouski@gmail.com>
2019-12-28 10:14:22 +0300

    Edit trustServiceUrl middleware

--- middleware/proactive.js
+++ middleware/proactive.js
@@ -2,14 +2,15 @@
  * @file Proactive messaging processing middleware.
  */
 const { MicrosoftAppCredentials } = require('botframework-connector');
-const { ActivityTypes } = require('botbuilder');
+// const { ActivityTypes } = require('botbuilder');
 
 /**
  * Restores service trust to the conversation activity.
  */
 module.exports.trustServiceUrl = async (context, next) => {
-    if (context.activity.type === ActivityTypes.Event) {
-        MicrosoftAppCredentials.trustServiceUrl(context.activity.serviceUrl);
-    }
+    // if (context.activity.type === ActivityTypes.Event || context.activity.type === ActivityTypes.Suggestion || context.activity.type === ActivityTypes.Message) {
+    //     MicrosoftAppCredentials.trustServiceUrl(context.activity.serviceUrl);
+    // }
+    MicrosoftAppCredentials.trustServiceUrl(context.activity.serviceUrl);
     await next();
 };
